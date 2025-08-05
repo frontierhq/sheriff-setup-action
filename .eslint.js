668ffbf050ac540beb57d762e29883b422d3c6bf
Dipak Shelar <dipak.shelar@mandg.com>
2025-08-05 14:19:41 +0530

    wip

--- .eslint.js
+++ .eslint.js
@@ -1,7 +1,6 @@
 module.exports = {
   root: true,
   extends: 'airbnb-base',
-  ignorePatterns: ['dist/'],
   rules: {
     'linebreak-style': ['error', (process.platform === 'win32' ? 'windows' : 'unix')],
   },
